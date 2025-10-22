e13adb0c6ca30d15e3572a7749b30f293eec19da
evemireya <emena@gbmpaulovi.edu.ec>
2025-10-22 16:32:06 -0500

    Agregar main.py
    Realicé un programa de un digito verificador hecho en python

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+numero_secreto = random.randint(1,50)
+print(numero_secreto)
+intento = 0
+adivinado = 0
+
+#Ciclo repetitivo
+while adivinado != numero_secreto:
+    #Intentos (incrementos y decrementos)
+    intento += 1
+    print(f"Este es tu intento {intento}, adivina el numero entre 1 y 50")
+    adivinado = int(input("Ingrese el numero"))
+    if intento == 5:
+        print(f"""Kaboooom. numero de intentos excedidos
+               Ha perdido""")
+        break 
